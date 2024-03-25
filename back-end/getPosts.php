7c40dc02e24a148fe98d33bae073410815338699
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-25 23:44:00 +0200

    adding get posts API

---
+++ back-end/getPosts.php
@@ -0,0 +1,37 @@
+<?php
+
+include('connection.php');
+
+
+$query = $mysqli->prepare(
+    'SELECT p.description ,u.name,u.bio 
+    FROM usersposts as p
+    INNER JOIN users as u ON p.user_id = u.id
+');
+
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows();
+
+if($num_rows == 0) {
+    $response["status"] = "No Posts";
+}else{
+    $posts = [];
+    $query->bind_result($description, $name, $bio);
+    while($query->fetch()){
+        $post = [
+            'description' => $description,
+            'name' => $name,
+            'bio' => $bio,
+        ];
+
+        $posts[] = $post;
+    }
+
+    $response["status"] = "Success";
+    $response["posts"] = $posts;
+}
+
+echo json_encode($response);
+
+
