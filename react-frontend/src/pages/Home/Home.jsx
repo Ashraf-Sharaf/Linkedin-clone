e03b15d4b4a31c5ffd616803b98b193a55198cad
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-25 22:43:26 +0200

    implementing the container tio view posts

--- react-frontend/src/pages/Home/Home.jsx
+++ react-frontend/src/pages/Home/Home.jsx
@@ -5,10 +5,24 @@ import "./home.css";
 
 const Home = () => {
   return (
- 
+    <div>
       <Header />
- 
-  
+      <div className="flex column center posts">
+        <div className="flex column post">
+          <div>
+            <div>User Name</div>
+            <div>user position</div>
+          </div>
+
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+            placeat, rerum rem sequi cum fuga ducimus assumenda ipsam eveniet
+            quidem itaque reiciendis doloremque consequuntur nisi, voluptates
+            amet nulla, ut quae?
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
