59c5db77fa0cdcf37181369af09b4ef84d665036
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-26 01:51:35 +0200

    adding the API of the login

---
+++ back-end/login.php
@@ -0,0 +1,35 @@
+<?php 
+include('connection.php');
+
+
+
+if (isset($_POST['email'], $_POST['password'])) {
+    
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = $mysqli->prepare(
+        'SELECT password 
+        FROM users 
+        WHERE email=?');
+    $query->bind_param('s', $email);
+    $query->execute();
+    $query->store_result();
+    $query->bind_result($pass);
+    $query->fetch();
+
+    if ($query->num_rows() == 1) {
+        if ($password == $pass) {
+            $response['status'] = "success";
+        } else {
+            $response['status'] = "failed"; 
+        }
+    } else {
+        $response['status'] = "failed"; 
+    }
+} else {
+    $response['status'] = "failed"; 
+}
+
+echo json_encode($response);
+?>
