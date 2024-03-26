68be8e4a2a3dc1342d475bd6a9923d878dc541d5
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-26 06:48:44 +0200

    adding the API to login Companies

---
+++ back-end/loginCompany.php
@@ -0,0 +1,37 @@
+<?php 
+include('connection.php');
+
+
+
+if (isset($_POST['email'], $_POST['password'])) {
+    
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = $mysqli->prepare(
+        'SELECT id,password 
+        FROM companies 
+        WHERE email=?');
+    $query->bind_param('s', $email);
+    $query->execute();
+    $query->store_result();
+    $query->bind_result($id,$pass);
+    $query->fetch();
+
+    if ($query->num_rows() == 1) {
+        if (password_verify($password, $pass)) {
+            $response['status'] = "success";
+            $response['id'] = $id;
+
+        } else {
+            $response['status'] = "failed"; 
+        }
+    } else {
+        $response['status'] = "failed"; 
+    }
+} else {
+    $response['status'] = "failed"; 
+}
+
+echo json_encode($response);
+?>
