78785440ed8b01c822e1b6028d41d659b17542eb
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-26 03:24:54 +0200

    adding the function to add a post using API

--- react-frontend/src/pages/Home/Home.jsx
+++ react-frontend/src/pages/Home/Home.jsx
@@ -6,7 +6,10 @@ import "./home.css";
 import PostCard from "./PostCard.jsx";
 
 const Home = () => {
+  const userID=localStorage.getItem("UserId");
 
+
+  const [description, setDescription] = useState("");
   const [posts, setPosts] = useState([]);
 
   const loadPosts = async () => {
@@ -14,31 +17,57 @@ const Home = () => {
       "http://localhost/linkedin/back-end/getPosts.php"
     );
 
-
-    setPosts(response.data['posts']);
+    setPosts(response.data["posts"]);
   };
 
   useEffect(() => {
     loadPosts();
   }, []);
 
-
   return (
     <div>
       <Header />
+
       <div className="flex column center posts">
-        
+        <div>
+          <input
+            type="text"
+            placeholder="description"
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          ></input>
+
+          <button
+            onClick={async () => {
+              try {
+                const formData = new FormData();
+                formData.append("description", description);
+                formData.append("id", userID);
+
+                const response = await axios.post(
+                  "http://localhost/linkedin/back-end/addPost.php",
+                  formData
+                );
+
+                if (response.data["status"] === "success") {
+                  alert("Post added");
+                }
+              } catch (error) {
+                console.error(error);
+              }
+            }}
+          >
+            Add Post
+          </button>
+        </div>
+
         {posts.map((post) => {
-        return <PostCard post={post} key={post.id} />;
-      })}
-        
+          return <PostCard post={post} key={post.id} />;
+        })}
       </div>
     </div>
   );
 };
 
 export default Home;
-
-
-
-
